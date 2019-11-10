67306b1d48934fb8677788e93550d447521a93dd
Yogesh <yogonweb@gmail.com>
2019-11-10 14:35:26 +0530

    RulesEngine-Primary Implementation

---
+++ RulesEngine/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace RulesEngine
+{
+    public partial class Form1 : Form
+    {
+        public ListDictionary myRulesList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myRulesList = new ListDictionary();
+
+            myRulesList.Add("LongTerm",new LongTermDiscount());
+            myRulesList.Add("BirthdayDiscount", new BirthdayDiscount());
+            myRulesList.Add("LastPurchaseDiscount", new LastPurchaseDiscount());
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            customer mycust= new customer();
+            float fTotalDiscount=0;
+
+            foreach(DictionaryEntry de in myRulesList)
+            {
+                fTotalDiscount += ((IRules)de.Value).Run(mycust);
+            }
+            Console.WriteLine("Total Disc " + fTotalDiscount.ToString());
+        }
+    }
+}
---
+++ RulesEngine/IRules.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RulesEngine
+{
+    interface IRules
+    {
+           float Run(customer cust); 
+    }
+
+    public class customer { };
+
+    public class LongTermDiscount : IRules
+    {
+        public float Run(customer cust)
+        {
+            return 10.0f;
+        }
+    }
+
+    public class BirthdayDiscount : IRules
+    {
+        public float Run(customer cust)
+        {
+            return 20.0f;
+        }
+    }
+
+    public class LastPurchaseDiscount : IRules
+    {
+        public float Run(customer cust)
+        {
+            return 15.0f;
+        }
+    }
+
+        
+}
